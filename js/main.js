e895569937380c856e7ba0e2471720566307c711
John Sidhom <johngrs15@gmail.com>
2017-12-13 14:48:05 +0200

    test 12

--- js/main.js
+++ js/main.js
@@ -1,27 +1,36 @@
 var app = {
-    registerEvents: function() {
-        var self = this;
-        // Check of browser supports touch events...
-        if (document.documentElement.hasOwnProperty('ontouchstart')) {
-            // ... if yes: register touch event listener to change the "selected" state of the item
-            $('body').on('touchstart', 'a', function(event) {
-                $(event.target).addClass('tappable-active');
-            });
-            $('body').on('touchend', 'a', function(event) {
-                $(event.target).removeClass('tappable-active');
-            });
-            $(window).on('hashchange', $.proxy(this.route, this));
+
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
         } else {
-            // ... if not: register mouse events instead
-            $('body').on('mousedown', 'a', function(event) {
-                $(event.target).addClass('tappable-active');
-            });
-            $('body').on('mouseup', 'a', function(event) {
-                $(event.target).removeClass('tappable-active');
-            });
-            
+            alert(title ? (title + ": " + message) : message);
         }
     },
+
+    registerEvents: function() {
+        $(window).on('hashchange', $.proxy(this.route, this));
+        $('body').on('mousedown', 'a', function(event) {
+            $(event.target).addClass('tappable-active');
+        });
+        $('body').on('mouseup', 'a', function(event) {
+            $(event.target).removeClass('tappable-active');
+        });
+    },
+
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(
+                message,
+                null, // callback
+                title,
+                'OK' // Button label
+            );
+        } else {
+            alert(title + ": " + message);
+        }
+    },
+
     route: function() {
         var hash = window.location.hash;
         if (!hash) {
@@ -35,6 +44,7 @@ var app = {
             });
         }
     },
+
     initialize: function() {
         var self = this;
         this.detailsURL = /^#employees\/(\d{1,})/;
@@ -42,14 +52,6 @@ var app = {
         this.store = new MemoryStore(function() {
             self.route();
         });
-    },
-
-    showAlert: function(message, title) {
-        if (navigator.notification) {
-            navigator.notification.alert(message, null, title, 'OK');
-        } else {
-            alert(title ? (title + ": " + message) : message);
-        }
     }
 
 };
